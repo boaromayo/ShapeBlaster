0032837525031d5cd000a1fb9e74c3ebd358f233
boaromayo <nly_poblet@yahoo.com>
2015-06-11 00:03:21 -0700

    Create BlasterConstants.java

---
+++ src/BlasterConstants.java
@@ -0,0 +1,11 @@
+package ShapeBlaster;
+import java.awt.*;
+
+public class BlasterConstants {
+	
+	// ASSIGN THE CONSTANTS.
+	public static final int BWIDTH = 640;
+	public static final int BHEIGHT = 480;
+	public static final Dimension BSIZE = new Dimension(BWIDTH, BHEIGHT);
+	
+}
