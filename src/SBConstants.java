d451bf2d991328ae11850fc0e62638858c3f67f6
boaromayo <nly_poblet@yahoo.com>
2015-06-11 23:05:30 -0700

    Update SBConstants.java

--- src/SBConstants.java
+++ src/SBConstants.java
@@ -3,7 +3,10 @@ import java.awt.*;
 
 public class SBConstants {
 	
-	// ASSIGN THE CONSTANTS.
+	// WAIT TIME CONSTANT.
+	public static final int EM_WAIT = 5;
+	
+	// DIMENSION SIZE CONSTANTS.
 	public static final int WIDTH = 640;
 	public static final int HEIGHT = 480;
 	public static final Dimension BSIZE = new Dimension(WIDTH, HEIGHT);
