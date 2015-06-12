3300676583c3e4d74fabdb54bec8f953ecfae798
boaromayo <nly_poblet@yahoo.com>
2015-06-11 22:05:28 -0700

    Update and rename BlasterConstants.java to SBConstants.java

---
+++ src/SBConstants.java
@@ -0,0 +1,11 @@
+package ShapeBlaster;
+import java.awt.*;
+
+public class SBConstants {
+	
+	// ASSIGN THE CONSTANTS.
+	public static final int WIDTH = 640;
+	public static final int HEIGHT = 480;
+	public static final Dimension BSIZE = new Dimension(WIDTH, HEIGHT);
+	
+}
